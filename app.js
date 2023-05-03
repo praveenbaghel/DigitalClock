93e7a3914bfc103746906d61e6484c90cdb2784f
Praveen Baghel <43009545+praveenbaghel@users.noreply.github.com>
2023-05-03 00:29:23 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+setInterval(currentTime, 1000);
+
+function currentTime() {
+    let time = new Date(); // creating object of Date class
+    let hour = time.getHours();
+    let min = time.getMinutes();
+    let sec = time.getSeconds();
+
+    var am_pm = "AM";
+    if (hour == 12)
+        am_pm = "PM";
+    if (hour > 12) {
+        hour -= 12;
+        am_pm = "PM";
+    }
+    if (hour == 0) {
+        hour = 12;
+        am_pm = "AM";
+    }
+
+    hour = hour < 10 ? "0" + hour : hour;
+    min = min < 10 ? "0" + min : min;
+    sec = sec < 10 ? "0" + sec : sec;
+
+    //value of current time
+    let currentTime = `${hour}:${min}:${sec} ${am_pm}`;
+    const clock = document.querySelector(".dispClock");
+    clock.innerHTML = currentTime;
+
+}
+
+currentTime();
